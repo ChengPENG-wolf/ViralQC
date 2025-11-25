89d2ae00de85e4b5453df6a77633776432ae7789
PENG Cheng <113765010+ChengPENG-wolf@users.noreply.github.com>
2025-11-25 23:57:01 +0800

    Add files via upload

--- viralqc.py
+++ viralqc.py
@@ -3,6 +3,7 @@ import os.path
 from utils.download_database import download_database
 from utils.contamination import contamination
 from utils.completeness import completeness
+from utils.custom_database import build_custom_database
 import argparse
 
 
@@ -11,13 +12,20 @@ def run_download_database(args):
     download_database(db_path)
 
 
+def run_build_custom_database(args):
+    fasta_file = args.fasta
+    database_pth = args.db
+    sequence_info_file = args.info
+    thread = args.threads
+    build_custom_database(fasta_file, database_pth, sequence_info_file, thread)
+
+
 def run_contamination(args):
     input = args.input
     db = args.db
     output = args.output
     threads = args.threads
     contamination(input, db, output, threads)
-
 def run_completeness(args):
     input = args.input
     db = os.path.join(args.db, 'genome_database')
@@ -40,6 +48,13 @@ if __name__ == '__main__':
     download_parser.add_argument('--db', type=str, help='The path to save the database.')
     download_parser.set_defaults(func=run_download_database)
 
+    custom_parser = subparsers.add_parser('build_custom_database', help='Build a custom database for ViralQC.')
+    custom_parser.add_argument('--fasta', '-f', type=str, help='The path to the custom viral genome fasta file.')
+    custom_parser.add_argument('--db', '-d', type=str, help='The path to save the custom database.')
+    custom_parser.add_argument('--info', '-s', type=str, default=None, help='The path to the sequence info file.')
+    custom_parser.add_argument('--threads', '-t', type=int, help='Number of threads.')
+    custom_parser.set_defaults(func=run_build_custom_database)
+
     contamination_parser = subparsers.add_parser('contamination', help='Detect and remove contamination in viral contigs.')
     contamination_parser.add_argument('--input', '-i', type=str, help='The path to the input fasta file.')
     contamination_parser.add_argument('--db', '-d', type=str, help='The path to the database.')
@@ -54,10 +69,6 @@ if __name__ == '__main__':
     completeness_parser.add_argument('--threads', '-t', type=int, help='Number of threads.')
     completeness_parser.add_argument('--bin', '-b', action='store_true', help='Evaluate the completeness of bins.')
     completeness_parser.set_defaults(func=run_completeness)
-
-
-
-    #end_to_end_parser = subparsers.add_parser('end_to_end', help='Run the end-to-end pipeline of ViralQC.')
 
     args = parser.parse_args()
     args.func(args)
