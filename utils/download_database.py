a98d607c23cb68db35705b3148e2a5a28d76548e
PENG Cheng <113765010+ChengPENG-wolf@users.noreply.github.com>
2026-03-17 00:41:38 +0800

    Add files via upload

--- utils/download_database.py
+++ utils/download_database.py
@@ -3,13 +3,16 @@ import gdown
 import os
 
 
-def download_database(db_path):
+def download_database(db_path, name):
 
     print(f'Downloading database to {db_path}')
     if not os.path.exists(db_path):
         os.makedirs(db_path)
 
-    id = "1WIcxTJfAFP6BbWYrATsD-tnZG5kzsxzc"
+    if name == 'refseq':
+        id = "1WIcxTJfAFP6BbWYrATsD-tnZG5kzsxzc"
+    elif name == 'meta':
+        id = '1Z73Y6rJwqIkWjePFxHvmuR6ujN7TNmyp'
     gdown.download(id=id, output=f'{db_path}/viralqc_db.tar.gz', quiet=False)
 
     cmd = f'tar -xzvf {db_path}/viralqc_db.tar.gz -C {db_path}'
